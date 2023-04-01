cbba532f48020a2b894326e65fb83e30a18d7d61
Drew Anello <drew.anello@gmail.com>
2023-04-01 16:05:30 -0400

    adds heros.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeroFormComponent } from './hero-form/hero-form.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+ imports: [
+  BrowserModule,
+  CommonModule,
+  FormsModule
+ ],
+
+ declarations: [
+  AppComponent,
+  HeroFormComponent
+ ],
+ providers: [],
+ bootstrap: [ AppComponent ]
 })
+
 export class AppModule { }
